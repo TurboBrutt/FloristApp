de22abe48a2de12decc586deecd72841e68777d5
Akhmed Israilov <chromium95@mail.ru>
2022-05-28 15:40:20 +0300

    fixed list

--- app/src/main/java/com/example/florist/FlowerFragment.java
+++ app/src/main/java/com/example/florist/FlowerFragment.java
@@ -4,15 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
 public class FlowerFragment extends Fragment {
+
+    private ListView lv;
+    private static String[] flowers={"Одуванчики","Тюльпаны",};
+
+    public static FlowerFragment newInstance(){
+        FlowerFragment flowerFragment=new FlowerFragment();
+
+        return flowerFragment;
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.flowerfragment,container,false);
+        View rootView = inflater.inflate(R.layout.flowerfragment, container, false);
+
+        lv= (ListView) rootView.findViewById(R.id.listviewflower);
+        lv.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_expandable_list_item_1,flowers));
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(),flowers[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
+    }
+
+    @Override
+    public String toString(){
+        return "FlowerFragment";
     }
 }
+
--- app/src/main/java/com/example/florist/TreeFragment.java
+++ app/src/main/java/com/example/florist/TreeFragment.java
@@ -1,18 +1,28 @@
 package com.example.florist;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class TreeFragment extends Fragment {
+
+    TextView linkTextView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.treefragment,container,false);
+        View rootView = inflater.inflate(R.layout.treefragment,container,false);
+
+        linkTextView = rootView.findViewById(R.id.txtLink);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        return rootView;
     }
+
 }
